ed37491cafc3a1504ed5e34f4ebbfd8424f35746
Mark Plant <gothicsyn@live.co.uk>
2021-02-20 16:47:57 +0000

    MovedToGitHub
    Moved from Azure to GitHub

---
+++ Assets/Scripts/Obsolete/DragDrop.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropZone;
+    private Vector2 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        isOverDropZone = true;
+        dropZone = other.gameObject;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+
+        if(isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Obsolete/DrawCards.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+
+  //  public GameObject card1;
+  //  public GameObject card2;
+    public GameObject playerArea;
+    public GameObject enemyArea;
+
+    public GameObject[] cardsArray;
+
+  //  List<GameObject> cards = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  //      cards.Add(card1);
+  //      cards.Add(card2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnClick()
+    {
+        for (var i = 0; i < 5; i++)
+        { 
+            // Populate the player cards
+            GameObject playerCard = Instantiate(cardsArray[Random.Range(0, cardsArray.Length)], new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(playerArea.transform, false);
+
+
+            //Populate the oppositions cards
+            GameObject enemyCard = Instantiate(cardsArray[Random.Range(0, cardsArray.Length)], new Vector3(0, 0, 0), Quaternion.Euler(0, 0, -180));
+            enemyCard.transform.SetParent(enemyArea.transform, false);
+        }
+    }
+}
