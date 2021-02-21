7add9a7aba841bc8bba264789005d71040e30270
Mark Plant <gothicsyn@live.co.uk>
2021-02-21 21:16:56 +0000

    VisualUpdates
    Visual Element layout began work on

---
+++ Assets/Scripts/Custom/CameraTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTest : MonoBehaviour
+{
+    public Transform target;
+    public Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+        Debug.Log("target is " + screenPos.x + " pixels from the left");
+    }
+}
